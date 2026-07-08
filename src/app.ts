9baa37a06bc0f6c03231df57d7494527a7daa9d9
MHMIZAN09 <221002550@student.green.edu.bd>
2026-07-08 15:49:08 +0600

    create a logger, notfound and global error handler

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,13 @@ import cors from 'cors';
 import express, { Request, Response } from 'express';
 import status from 'http-status';
 
+import fs from 'fs';
+import path from 'path';
+import swaggerUI from 'swagger-ui-express';
+import YAML from 'yaml';
 import config from './config';
+import { globalErrorHandler } from './middlewares/globalErrorHandler';
+import notFound from './middlewares/notFound';
 import { indexRoutes } from './routes';
 
 const app = express();
@@ -18,6 +24,15 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+// app.use(logger);
+
+// Swagger YAML file read
+const swaggerFilePath = path.join(process.cwd(), 'src', 'docs', 'swagger.yml');
+const swaggerFile = fs.readFileSync(swaggerFilePath, 'utf8');
+const swaggerDocument = YAML.parse(swaggerFile);
+
+// Swagger route
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.get('/', (req: Request, res: Response) => {
   res.status(status.OK).json({
@@ -29,4 +44,6 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use('/api', indexRoutes);
 
+app.use(notFound);
+app.use(globalErrorHandler);
 export default app;
